f71b5fb66594aaddd3828055fe9af7be840a8970
yupeng1 <yupeng1@100tal.com>
2019-09-10 20:21:02 +0800

    feat: 项目路由 引入markdown插件 增加打包时静态资源路径判断

--- teamwork-note-demo/fe/teamwork-note/src/main.js
+++ teamwork-note-demo/fe/teamwork-note/src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import './normalize.css'
+import mavonEditor from 'mavon-editor'
+import 'mavon-editor/dist/css/index.css'
 
+Vue.use(mavonEditor)
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  router,
 }).$mount('#app')
---
+++ teamwork-note-demo/fe/teamwork-note/src/pages/edit/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <mavon-editor v-model="value" />
+  </div>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
